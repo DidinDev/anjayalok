4815fa325e2cca8e1bba29d84327799ca6870d41
blakvlalckakcksk <113559182+DidinDev@users.noreply.github.com>
2022-10-15 16:50:08 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ app.listen(PORT, () => {
 	██╔══██╗██╔══╝  ╚════██║   ██║   ██╔══██║██╔═══╝ ██║
 	██║  ██║███████╗███████║   ██║   ██║  ██║██║     ██║
 	╚═╝  ╚═╝╚══════╝╚══════╝   ╚═╝   ╚═╝  ╚═╝╚═╝     ╚═╝
-			                              Make by Alip 
+			                              Make by Didin 
 								 
  Server running on http://localhost:` + PORT)
 console.log(`Hello ${creator}`)
